412659b31b73dde1a88466d0830d6283f8018a3d
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-06-23 19:39:00 +0300

    updated API call and render functions

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 
 const DEFAULT_PER_PAGE = 15;
+const API_KEY = '50948492-7a2c95c1e3ac536ddbec900a1';
 
-export const getImagesByQuery = async (query, page = 1) => {
+export async function getImagesByQuery(query, page = 1) {
   try {
     const response = await axios.get('https://pixabay.com/api/', {
-      query: {
-        key: 'TODO',
+      params: {
+        key: API_KEY,
         q: query,
         page,
         per_page: DEFAULT_PER_PAGE,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
       },
     });
 
-    return response.hits;
+    return response.data;
   } catch (error) {
     console.error(`Failed to fetch images by query ${query}.`, error);
   }
-};
+}
